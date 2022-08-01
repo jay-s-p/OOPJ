f178153540ec560b691ff9612baf7749320f34df
jay <jaysparmar303@gmail.com>
2022-08-01 20:27:18 +0530

    Added Scanner Program

---
+++ Scanner/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        
+        String name;
+        System.out.print("Enter your name : ");
+        name = new Scanner(System.in).nextLine();
+        System.out.println("Your name is " + name);
+
+    }
+}
