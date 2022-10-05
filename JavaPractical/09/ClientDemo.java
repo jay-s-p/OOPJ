0d9a811b943ce12cc0f3b447723121139224e345
Jay Parmar <jaysparmar303@gmail.com>
2022-10-05 18:40:55 +0530

    .

--- JavaPractical/09/ClientDemo.java
+++ JavaPractical/09/ClientDemo.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 
 class ClientDemo {
     public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
         DatagramSocket ds = new DatagramSocket();
         InetAddress ip = InetAddress.getLocalHost();
         byte data[] = null;
 
         while (true) {
-            String input = new Scanner(System.in).nextLine();
+            String input = sc.nextLine();
             data = input.getBytes();
             DatagramPacket dp = new DatagramPacket(data, data.length, ip, 1234);
             ds.send(dp);
