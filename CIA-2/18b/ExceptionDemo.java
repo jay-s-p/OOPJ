68f30c3a76a458b50faf756e626946090b2c3d3e
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2022-10-30 15:15:05 +0530

    added 18b prog to CIA-2

---
+++ CIA-2/18b/ExceptionDemo.java
@@ -0,0 +1,31 @@
+/*
+ * Write a program to create a user-defined exception MyException.
+ * Define a class ExceptionDemo that has a method named compute()
+ * which throws MyException object, when compute()'s integer parameter 
+ * is grater than 10.
+ * 
+ */
+
+class MyException extends Exception {
+    MyException(String msg) {
+        super(msg);
+    }
+}
+
+public class ExceptionDemo {
+    void compute(int x) throws MyException {
+        if (x > 10)
+            throw new MyException("Value must be less than 10.");
+    }
+
+    public static void main(String[] args) {
+        ExceptionDemo demo = new ExceptionDemo();
+        try {
+            demo.compute(0);
+            demo.compute(7);
+            demo.compute(11);
+        } catch (MyException e) {
+            System.out.println(e);
+        }
+    }
+}
